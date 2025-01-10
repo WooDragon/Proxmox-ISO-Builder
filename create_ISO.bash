25832c2b47ffc284bb8a8626af3a09286bbbde50
WooDragon <mutoulong@gmail.com>
2025-01-10 16:33:14 +0800

    Update create_ISO.bash

--- create_ISO.bash
+++ create_ISO.bash
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-mkdir temp
-mount debian* temp
+# 1) 准备工作目录
+mkdir -p temp
+mount debian-*.iso temp
 cp -a temp/. iso
 chmod -R 644 iso
 umount temp
 
+# 2) 把预置文件和离线仓库复制到 iso 根目录
 cp preseed.cfg ./iso
 
-sed -i "s+quiet+quiet priority=high locale=en_US.UTF-8 keymap=de file=/cdrom/preseed.cfg+g" iso/isolinux/txt.cfg iso/boot/grub/grub.cfg
+# 假设 GitHub Actions 的"pve"目录和脚本在同级目录下
+cp -r pve iso/
 
-OUTPUT=proxmox_custom.iso
+# 3) 修改 isolinux/grub 的启动选项，让安装器加载 preseed.cfg
+sed -i "s+quiet+quiet priority=high locale=en_US.UTF-8 keymap=de file=/cdrom/preseed.cfg+g" \
+  iso/isolinux/txt.cfg iso/boot/grub/grub.cfg
+
+# 4) 生成自定义 ISO
+OUTPUT="proxmox_custom.iso"
 xorriso \
    -outdev "$OUTPUT" \
-   -volid "$OUTPUT" \
+   -volid "Proxmox_Custom" \
    -padding 0 \
    -compliance no_emul_toc \
    -map "./iso" / \
@@ -24,4 +32,4 @@ xorriso \
    -boot_image isolinux partition_entry=gpt_basdat \
    -stdio_sync off
 
-isohybrid --uefi $OUTPUT
+isohybrid --uefi "$OUTPUT"
