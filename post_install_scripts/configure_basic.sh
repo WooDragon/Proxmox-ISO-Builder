a9b8d3c889e0ac39a79b8f4034cf089796217a41
WooDragon <mutoulong@gmail.com>
2025-01-19 21:06:55 +0800

    Update and rename configure_hosts.sh to configure_basic.sh

---
+++ post_install_scripts/configure_basic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "127.0.0.1 proxmox.local proxmox" >> /etc/hosts
+# 配置 /etc/fstab
+sed -i 's/\(.*\) \{1,\}[0-9]\+$/\10/' /etc/fstab
+# 创建 systemd override 文件以设置 TimeoutStartSec
+mkdir -p /etc/systemd/system/networking.service.d
+echo -e '[Service]\nTimeoutStartSec=90s' > /etc/systemd/system/networking.service.d/override.conf
+# 配置 APT 源列表
+echo 'deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware' > /etc/apt/sources.list
+echo 'deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+echo 'deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+echo 'deb https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware' >> /etc/apt/sources.list
+# 移除 Proxmox Enterprise 源列表文件
+rm -f /etc/apt/sources.list.d/pve-enterprise.list || true
+# 添加 Proxmox no-subscription 源
+echo 'deb http://mirrors.ustc.edu.cn/proxmox/debian/pve bookworm pve-no-subscription' > /etc/apt/sources.list.d/pve-install-repo.list
--- post_install_scripts/configure_hosts.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "127.0.0.1 proxmox.local proxmox" >> /etc/hosts
