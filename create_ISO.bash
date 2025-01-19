2ae9f88ac55f7d3e1921260b448f94fde41b6f43
WooDragon <mutoulong@gmail.com>
2025-01-19 22:07:08 +0800

    Update create_ISO.bash

--- create_ISO.bash
+++ create_ISO.bash
@@ -198,8 +198,7 @@ EOF
 # 4.2 UEFI grub.cfg
 sed -i '/set timeout=/s/.*/set timeout=10/' "$WORKDIR/iso/boot/grub/grub.cfg"
 sed -i '/set default=/c\set default=\"Automated Installation\"' "$WORKDIR/iso/boot/grub/grub.cfg"
-grep 'menuentry' "$WORKDIR/iso/boot/grub/grub.cfg"
-grep 'set default=' "$WORKDIR/iso/boot/grub/grub.cfg"
+cat "$WORKDIR/iso/boot/grub/grub.cfg"
 
 cat <<EOF >> "$WORKDIR/iso/boot/grub/grub.cfg"
 
